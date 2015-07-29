98dbd10e32754fa7335e253a841a05636613a388
scKeijser <tobias.keijser@studentconsulting.com>
2015-07-29 14:25:10 +0200

    Changed to hello Name

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,9 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + "!");
             Console.ReadKey();
         }
     }
